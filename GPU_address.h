d060de9db195016ddf705f4c57910fa3b0071818
Davi Oliveira <155112126+DaviOSC@users.noreply.github.com>
2024-10-10 10:05:31 -0300

    Add files via upload

---
+++ GPU_address.h
@@ -0,0 +1,13 @@
+//Barramentos da GPU
+#define DATA_A 0X70
+#define DATA_B 0X80
+#define RESET_PULSECOUNTER 0X90
+#define SCREEN 0XA0
+#define WRFULL 0XB0
+#define WRREG 0XC0
+
+//Mapeamento de memória
+
+#define ALT_LWFPGASLVS_OFST 0xff200000
+#define HW_REGS_BASE 0xfc000000
+#define HW_REGS_SPAN 0x04000000
